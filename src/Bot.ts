cca12cd1909c50f333f7dd4fe3c29779c2f508d6
pan93412 <pan93412@gmail.com>
2021-01-01 22:44:39 +0800

    refactor(Bot): make code clear

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,58 +2,61 @@ import Discord from 'discord.js';
 import Message from './Message';
 
 export default class Bot {
-    instance: Bot | undefined;
+  instance: Bot | undefined;
 
-    identifier: string = 'ciscc';
+  identifier: string = 'ciscc';
 
-    client: Discord.Client | undefined;
+  client: Discord.Client | undefined;
 
-    channel: Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel | undefined;
+  channel: Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel | undefined;
 
-    constructor() {
-        if (this.instance) return this.instance;
+  constructor() {
+    if (this.instance) return this.instance;
 
-        this.instance = this;
-        this.client = new Discord.Client();
-        this.client.on('ready', () => {
-            if (this.client)
-                console.log(`info: logged in as ${this.client.user?.tag}`);
-        });
-        this.client.on('message', this.registerChannelHandler(this));
-    }
+    this.instance = this;
+    this.client = new Discord.Client();
+    this.client.on('ready', () => {
+      if (this.client) console.log(`info: logged in as ${this.client.user?.tag}`);
+    });
 
-    private registerChannelHandler(instance: Bot) {
-        return function (message: Discord.Message) {
-            if (message.content === `!${instance.identifier}::registerChannel`) {
-                if (instance.checkPermission(message.member, 'MANAGE_ROLES')) {
-                    instance.channel = message.channel;
-                    message.reply(instance.buildNotification('已經成功設定預設發言平台。')); // TODO: i18n
-                    console.info(`info: the default text channel has been set to ${instance.channel.id}`)
-                } else {
-                    message.reply(instance.buildNotification('您無權執行本動作。')); // TODO: i18n
-                }
-            }
-        }
-    }
+    this.registerChannelHandler();
+  }
 
-    checkPermission(member: Discord.GuildMember | null, permission: Discord.PermissionString): boolean {
-        if (member) return member.hasPermission(permission);
-        return false;
-    }
-
-    sendMessageObject(message: Message) {
-        if (this.channel) {
-            this.channel.send(message.toString());
+  private registerChannelHandler() {
+    this.client?.on('message', (message) => {
+      if (message.content === `!${this.identifier}::registerChannel`) {
+        if (Bot.checkPermission(message.member, 'MANAGE_ROLES')) {
+          this.channel = message.channel;
+          message.reply(this.buildNotification('已經成功設定預設發言平台。')); // TODO: i18n
+          console.info(`info: the default text channel has been set to ${this.channel.id}`);
         } else {
-            console.warn('warning: nobody specified the text channel to send.')
+          message.reply(this.buildNotification('您無權執行本動作。')); // TODO: i18n
         }
-    }
+      }
+    });
+  }
 
-    buildNotification(message: string) {
-        return `${message} (${this.identifier})`;
-    }
+  static checkPermission(
+    member: Discord.GuildMember | null,
+    permission: Discord.PermissionString,
+  ): boolean {
+    if (member) return member.hasPermission(permission);
+    return false;
+  }
 
-    async login(token: string): Promise<string | undefined> {
-        return await this.client?.login(token);
+  sendMessageObject(message: Message) {
+    if (this.channel) {
+      this.channel.send(message.toString());
+    } else {
+      console.warn('warning: nobody specified the text channel to send.');
     }
+  }
+
+  buildNotification(message: string) {
+    return `${message} (${this.identifier})`;
+  }
+
+  async login(token: string): Promise<string | undefined> {
+    return this.client?.login(token);
+  }
 }
