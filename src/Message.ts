43bb42a77752fbf7b25d299aa6d1efcce6f5a66c
pan93412 <pan93412@gmail.com>
2021-01-01 22:45:43 +0800

    style(Message): spacing

--- src/Message.ts
+++ src/Message.ts
@@ -10,11 +10,11 @@ export default class Message {
     // IMPL: Unix Timestamp?
     /** @unused */
     submittedAt: number = Date.now();
-    
+
     /** @unused */
     reviewedAt: number = Date.now();
 
     toString() {
-        return `[匿名] ${this.message}`;
+      return `[匿名] ${this.message}`;
     }
 }
