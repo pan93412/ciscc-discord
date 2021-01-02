6d18dfd0a3e9729e66502e49edc4fb06ee7ace16
pan93412 <pan93412@gmail.com>
2021-01-02 14:03:28 +0800

    refactor: move index.ts into ./src

--- index.ts
+++
@@ -1,9 +0,0 @@
-import Bot from './src/Bot';
-import Message from './src/Message';
-import MessageBuilder from './src/MessageBuilder';
-
-export default {
-  Bot,
-  Message,
-  MessageBuilder,
-};
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import Bot from './Bot';
+import Message from './Message';
+import MessageBuilder from './MessageBuilder';
+
+export default {
+  Bot,
+  Message,
+  MessageBuilder,
+};
