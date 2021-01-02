c6d6d88b09114d54d662a3013bb5f9e5a5ccd63c
pan93412 <pan93412@gmail.com>
2021-01-03 01:19:00 +0800

    feat(Message): remove the useless Id

--- src/Message.ts
+++ src/Message.ts
@@ -1,11 +1,7 @@
-import crypto from 'crypto';
-
 /**
  * A complaint message to send to Discord.
  */
 export default class Message {
-    static hash = crypto.createHash('sha256');
-
     /** The UTF-8 message to be sent. */
     message = '';
 
@@ -19,17 +15,11 @@ export default class Message {
     /** @unused */
     reviewedAt: number = Date.now();
 
-    get Id(): string {
-      Message.hash.update(`${this.message}${this.submittedAt.toString()}`);
-      return Message.hash.digest('hex');
-    }
-
     toString(): string {
       return [
         `[匿名] ${this.message}`,
         '',
         `發表時間：${new Date(this.submittedAt).toLocaleString()}`,
-        `訊息的唯一 ID：${this.Id}`,
       ].join('\r\n');
     }
 }
