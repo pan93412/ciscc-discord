9dc61724f5dffba29ebfb2b7e2b30741dc760002
pan93412 <28441561+pan93412@users.noreply.github.com>
2021-01-15 06:43:30 +0800

    style(MessageBuilder): methods should start as the lowercase letter

--- src/MessageBuilder.ts
+++ src/MessageBuilder.ts
@@ -1,5 +1,5 @@
 import Message from './Message';
-import RemoveMentions from './utils/RemoveMentions';
+import removeMentions from './utils/RemoveMentions';
 
 /**
  * The builder for @see Message
@@ -23,6 +23,6 @@ export default class MessageBuilder {
     }
 
     build(): Message {
-      return RemoveMentions(this.message);
+      return removeMentions(this.message);
     }
 }
